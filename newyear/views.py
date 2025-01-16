49781dce7f5aeb47ad6d03fdba08c191995bc4c2
Wong Zheng Yew <wong_zhengyew@mymail.sutd.edu.sg>
2025-01-16 17:47:37 +0800

    updated newyear app

--- newyear/views.py
+++ newyear/views.py
@@ -1,3 +1,16 @@
+
 from django.shortcuts import render
 
-# Create your views here.
+import datetime
+
+
+
+def index(request):
+
+    now = datetime.datetime.now()
+
+    return render(request, "newyear/index.html", {
+
+        "newyear": now.month == 1 and now.day == 1
+
+    })
